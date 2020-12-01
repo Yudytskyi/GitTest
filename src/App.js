cce06984676cd36bbd47d3b662b96c6320db0d87
Yudytskyi <uan0503631989@gmail.com>
2020-12-01 18:44:37 +0200

    Add comp3 in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Comp1 from './components/Comp1.js'
 import Comp2 from './components/Comp2.js'
+import Comp3 from './components/Comp3.js'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <h2>change 2</h2>
 <Comp1/>
 <Comp2/>
+<Comp3/>
     </>
   );
 }
--- src/components/Comp3.js
+++ src/components/Comp3.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Comp3 = () => {
     return (
         <h1>
-            component 2
+            component 3 branch2
         </h1>
     )
 }
