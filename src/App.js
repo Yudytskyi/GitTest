b93bda7361867a425634ff93a8828da7970d2b00
Yudytskyi <uan0503631989@gmail.com>
2020-12-01 18:37:52 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Comp1 from './components/Comp1.js'
+import Comp2 from './components/Comp2.js'
 
 function App() {
   return (
@@ -5,10 +7,10 @@ function App() {
     <h1>Hello, World! I`m is Git</h1>
     <h2>change 1</h2>
     <h2>change 2</h2>
+<Comp1/>
+<Comp2/>
     </>
   );
 }
 
 export default App;
-
-// lajkdshfljahsdf"
---
+++ src/components/Comp2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Comp2 = () => {
+    return (
+        <h1>
+            component 2
+        </h1>
+    )
+}
+
+export default Comp2;
