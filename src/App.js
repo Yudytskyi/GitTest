35e740377f5274b3e83e00afea67f04fbae8b063
Yudytskyi <uan0503631989@gmail.com>
2020-12-05 13:52:34 +0200

    add Comp6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Comp1 from './components/Comp1.js'
 import Comp3 from './components/Comp3.js'
 import Comp4 from './components/Comp4.js'
+import Comp6 from './components/Comp6.js'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 <Comp1/>
 <Comp3/>
 <Comp4/>
+<Comp6/>
     </>
   );
 }
---
+++ src/components/Comp6.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Comp6 = () => {
+    return (
+        <h1>
+            component 6 main
+        </h1>
+    )
+}
+
+export default Comp6;
